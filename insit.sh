1dc7196219843c9d2342c9d0ca3a6dc58752b8d1
dmcimi <1529209915@qq.com>
2022-02-14 04:16:29 +0800

    ServerStatus

--- insit.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-nohup /etc/init.d/status start && /ServerStatus/server/sergate --config=/ServerStatus/server/config.json --port=3561 --web-dir=/usr/share/html
-
-/etc/init.d/status start 
-nohup /ServerStatus/server/sergate --config=/ServerStatus/server/config.json --port=3561 --web-dir=/usr/share/html 
