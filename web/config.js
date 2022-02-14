bb6fb3c7492ad9592f27ce2f42931e5bef487610
Dmcimi <61643603+dmcimi@users.noreply.github.com>
2022-02-14 10:39:12 +0800

    Update config.js

--- web/config.js
+++ web/config.js
@@ -2,7 +2,7 @@ window.__PRE_CONFIG__ = {
   header: 'Server Status',
   subHeader: 'Servers\' Probes Set up with ServerStatus',
   interval: 1.5,
-  footer: '<p>© 2022 <a href="https://akiaki.top">akiaki</a>Powered by <a href="https://github.com/cokemine/ServerStatus-Hotaru">Server Status</a></p>'
+  footer: '<p>© 2022 <a href=""></a>Powered by <a href="https://github.com/cokemine/ServerStatus-Hotaru">Server Status</a></p>'
 };
 
 
